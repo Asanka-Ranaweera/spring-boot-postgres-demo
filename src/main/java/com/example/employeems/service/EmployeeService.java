7cb00eb207837348cca14d89ad891585022cd1ce
Asanka Ranaweera <asankapranaweera@gmail.com>
2025-09-19 12:06:00 +0530

    initial commit-save method

---
+++ src/main/java/com/example/employeems/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.employeems.service;
+
+import com.example.employeems.dto.EmployeeDTO;
+import com.example.employeems.dto.ResponseDTO;
+import com.example.employeems.entity.Employee;
+import com.example.employeems.repo.EmployeeRepo;
+import com.example.employeems.util.VarList;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public String saveEmployee(EmployeeDTO employeeDTO) {
+        if(employeeRepo.existsById(employeeDTO.getEmpID())){
+            return VarList.RSP_DUPLICATED;                          //send to controller
+        }else {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+        }
+    }
+}
