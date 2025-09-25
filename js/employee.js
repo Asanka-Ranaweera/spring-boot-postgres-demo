ba775d0d24d2954364afbfa33080c2bbbd89ac19
Asanka Ranaweera <asankapranaweera@gmail.com>
2025-09-26 00:10:19 +0530

    initial

---
+++ js/employee.js
@@ -0,0 +1,27 @@
+function saveEmployee(){
+    let name = $('#exampleFormControlInput2').val();
+    let address = $('#exampleFormControlInput3').val();
+    let number = $('#exampleFormControlInput4').val();
+
+    //console.log(name)
+
+    $.ajax({
+        method:"POST",
+        contentType:"application/json",
+        url:"http://localhost:8080/api/v1/employee/saveEmployee",
+        async:true,
+        data:JSON.stringify({
+            "empID" :"",
+            "empName": name,
+            "empAddress": address,
+            "empMNumber": number
+        }),
+        success:function (data){
+            alert("Saved")
+        },
+        error:function (xhr, exception){
+            alert("Error")
+        }
+    })
+
+}
