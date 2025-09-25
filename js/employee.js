3dcf450032d69f585d8e395cbed476d17fcea4eb
Asanka Ranaweera <asankapranaweera@gmail.com>
2025-09-26 03:03:28 +0530

    all part complete

--- js/employee.js
+++ js/employee.js
@@ -1,3 +1,4 @@
+getAllEmployee();
 function saveEmployee(){
     let name = $('#exampleFormControlInput2').val();
     let address = $('#exampleFormControlInput3').val();
@@ -18,6 +19,7 @@ function saveEmployee(){
         }),
         success:function (data){
             alert("Saved")
+            getAllEmployee()
         },
         error:function (xhr, exception){
             alert("Error")
@@ -25,3 +27,91 @@ function saveEmployee(){
     })
 
 }
+
+function updateEmployee(){
+    let empID = $('#exampleFormControlInput1').val();
+    let name = $('#exampleFormControlInput2').val();
+    let address = $('#exampleFormControlInput3').val();
+    let number = $('#exampleFormControlInput4').val();
+
+    $.ajax({
+        method:"PUT",
+        contentType:"application/json",
+        url:"http://localhost:8080/api/v1/employee/updateEmployee",
+        async:true,
+        data:JSON.stringify({
+            "empID" : empID,
+            "empName": name,
+            "empAddress": address,
+            "empMNumber": number
+        }),
+        success:function (data){
+            alert("Updated")
+            getAllEmployee()
+        },
+        error:function (xhr, exception){
+            alert("Error")
+        }
+    })
+
+}
+
+function deleteEmployee(){
+    let empID = $('#exampleFormControlInput1').val();
+
+    $.ajax({
+        method:"DELETE",
+        contentType:"application/json",
+        url:"http://localhost:8080/api/v1/employee/deleteEmployee/"+empID,
+        async:true,
+        success:function (data){
+            alert("Deleted")
+            getAllEmployee()
+        },
+        error:function (xhr, exception){
+            alert("Error")
+        }
+    })
+
+}
+
+function getAllEmployee(){
+
+    $.ajax({
+        method:"GET",
+        contentType:"application/json",
+        url:"http://localhost:8080/api/v1/employee/getAllEmployees",
+        async:true,
+        success:function (data){
+            if(data.code === "00"){
+                $('#empTable').empty();
+                for (let emp of data.content){
+                    let empID = emp.empID
+                    let name = emp.empName
+                    let address= emp.empAddress
+                    let number = emp.empMNumber
+
+                    var row = `<tr><td>${empID}</td><td>${name}</td><td>${address}</td><td>${number}</td></tr>`;
+                    $('#empTable').append(row);
+                }
+            }
+        },
+        error:function (xhr, exception){
+            alert("Error")
+        }
+    })
+
+}
+$(document).ready(function (){
+    $(document).on('click', '#empTable tr', function () {
+        var col0 = $(this).find('td:eq(0)').text();
+        var col1 = $(this).find('td:eq(1)').text();
+        var col2 = $(this).find('td:eq(2)').text();
+        var col3 = $(this).find('td:eq(3)').text();
+
+        $('#exampleFormControlInput1').val(col0);
+        $('#exampleFormControlInput2').val(col1);
+        $('#exampleFormControlInput3').val(col2);
+        $('#exampleFormControlInput4').val(col3);
+    })
+})
